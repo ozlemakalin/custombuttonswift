65fdbd887771aa12df7c8a6d11c7bc4621874eb2
Özlem Akalın <ozlem_akalin685@hotmail.com>
2015-01-24 13:42:18 +0200

    Project files

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Ozlem Akalin on 24/01/15.
+//  Copyright (c) 2015 Ozlem Akalin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
